0ccbec78755d0584214badb9d6183615ff0a5015
vivekthorat73 <120593033+vivekthorat73@users.noreply.github.com>
2025-06-06 18:22:32 +0530

    Handle unmatched routes draft 1

--- secure-land-registry/client/src/App.js
+++ secure-land-registry/client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import ListYourLand from "./pages/ListYourLand";
@@ -15,21 +15,20 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-
         <Route path="/" element={<LoginPage />} />
-
         <Route path="/profile" element={<Profile />} />
         <Route path="/list-land" element={<ListYourLand />} />
         <Route path="/admin/land-approval" element={<AdminApproval />} />
         <Route path="/lands" element={<ListOfLands />} />
         <Route path="/land/:id" element={<LandDetails />} />
-        <Route path="/land/:id" element={<LandInquiryPage />} />
+        <Route path="/land/:id/inquire" element={<LandInquiryPage />} />
         <Route path="/chat" element={<ChatPage />} />
         <Route path="/transactions" element={<TransactionHistory />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
 
+        {/* Handle unmatched routes */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
   );
